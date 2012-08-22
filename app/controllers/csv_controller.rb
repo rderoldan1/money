6236df2ff7092a4a7715e1e170da905904cf42fb
Luke Thomas <luk3thomas@gmail.com>
2012-08-22 00:08:04 -0500

    update csv uploader to parse csv file from upload

--- app/controllers/csv_controller.rb
+++ app/controllers/csv_controller.rb
@@ -1,19 +1,29 @@
 class CsvController < ApplicationController
 
+  def upload
+  end
+
   def import
 
     require 'csv'
     require 'date'
     require 'digest'
 
-    CSV.foreach( params[:bank_transaction][:csv], { :headers => true } ) do |row|
+    CSV.new( params[:upload][:csv].tempfile, { :headers => true } ).each do |row|
+
       out = {}
-      out["sha1_digest"] = Digest::SHA1::hexdigest row.to_s
-      row["No."].strip!
-      row["Date"] = Date.strptime(row["Date"], '%m/%d/%Y')
-                        .strftime('%Y-%m-%d %H:%M:%S')
+      out["date"]         = Date.strptime(row["Date"], '%m/%d/%Y')
+                                .strftime('%Y-%m-%d %H:%M:%S')
+      out["check_number"] = row["No."].strip
+      out["description"]  = row["Description"]
+      out["debit"]        = row["Debit"]
+      out["credit"]       = row["Credit"]
+      out["sha1_digest"]  = Digest::SHA1::hexdigest row.to_s
+
+      BankTransaction.create out
+
     end
-    
+
   end
 
 end
