d390a7f37c86bae130938f326516fa3af72b9720
Luke Thomas <luk3thomas@gmail.com>
2012-08-21 22:33:24 -0500

    add sha1_digest, date, and description validation add rspec tests

--- app/models/bank_transaction.rb
+++ app/models/bank_transaction.rb
@@ -1,3 +1,8 @@
 class BankTransaction < ActiveRecord::Base
-  attr_accessible :check_number, :credit, :date, :debit, :description
+  attr_accessible :check_number, :credit, :date, :debit, :description, :sha1_digest
+
+  validates :date, :presence => true
+  validates :description, :presence => true
+  validates :sha1_digest, :presence => true, :length => { :is => 40 }, :uniqueness => true
+
 end
--- spec/models/bank_transaction_spec.rb
+++ spec/models/bank_transaction_spec.rb
@@ -1,5 +1,63 @@
 require 'spec_helper'
+require 'digest'
+require 'date'
 
 describe BankTransaction do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    fingerprint = Digest::SHA1::hexdigest "a"
+    @t = BankTransaction.new( date: Time.now, description: 'Some', sha1_digest: fingerprint ) 
+  end
+
+  subject { @t }
+
+  it { should respond_to(:date) }
+  it { should respond_to(:description) }
+  it { should respond_to(:sha1_digest) }
+
+  describe "invalid fingerprint" do
+
+    it "not long enough" do
+      @t.sha1_digest = 'a'
+      @t.should_not be_valid
+    end
+
+    it "too long" do
+      @t.sha1_digest = 'a' * 41
+      @t.should_not be_valid
+    end
+
+    it "not present" do
+      @t.sha1_digest = ''
+      @t.should_not be_valid
+    end
+
+    it 'not unique' do 
+      dup = @t.dup
+      dup.save
+
+      should_not be_valid
+
+    end
+
+  end
+
+  describe "invalid date" do
+
+    it 'not present' do
+      @t.date = nil
+      @t.should_not be_valid
+    end
+
+  end
+
+  describe "invalid description" do
+
+    it 'not present' do
+      @t.description = ''
+      @t.should_not be_valid
+    end
+
+  end
+
 end
