3a079288dcc0ce05a18e0c0f830256e64a6c865b
Luke Thomas <luk3thomas@gmail.com>
2013-02-04 20:28:04 -0600

    remove spacing

--- app/controllers/flow_controller.rb
+++ app/controllers/flow_controller.rb
@@ -8,32 +8,19 @@ class FlowController < ApplicationController
   end
 
   def update
-
     params[:transactions].each do |k, v|
-
       if v[:descriptor].empty?
         next
       end
-
       id = k.split('-')[1].to_i
-
       @transaction = Transaction.find( id )
-
       if @transaction.meta
-
         @transaction.meta.update_attributes( v )
-
       else
-
         @transaction.create_meta! v
-
       end
-
-
     end
-
     #redirect_to flow_edit_path
-
   end
 
   def edit
